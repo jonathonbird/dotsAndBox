966412a5e453dbc04b3864bf9e48c709dba1ebc0
HusamAQ <56165481+HusamAQ@users.noreply.github.com>
2020-09-15 15:24:22 +0200

    aa

--- src/graphics/MainMenu.java
+++ src/graphics/MainMenu.java
@@ -1,5 +1,16 @@
 package graphics;
 
-public class MainMenu {
+import javax.swing.JFrame;
+import javax.swing.JPanel;
 
+public class MainMenu {
+	private JFrame frame;
+	private JPanel MainMenuPanel;
+
+	public MainMenu(JFrame frame) {
+		this.frame=frame;
+		this.MainMenuPanel=new JPanel();
+	}
+	
+	private void setUp() {}
 }
