ee6e488dc25c3228267c5a42d41d3a811eaeaf1c
HusamAQ <56165481+HusamAQ@users.noreply.github.com>
2020-09-15 14:24:45 +0200

    basic set up

--- src/graphics/MenuBasic.java
+++ src/graphics/MenuBasic.java
@@ -1,5 +1,30 @@
 package graphics;
 
-public class MenuBasic {
+import javax.swing.JFrame;
+import javax.swing.JPanel;
 
+public class MenuBasic {
+	private JFrame frame;
+	private JPanel panel;
+	
+	public MenuBasic() {
+		this.frame=new JFrame();
+		frame.setSize(Paths.FRAME_WIDTH, Paths.FRAME_HEIGHT);
+		frame.setResizable(false);
+		this.panel=new JPanel();
+		frame.add(panel);
+	}
+	
+	public JFrame getFrame() {
+		return frame;
+	}
+	
+	public JPanel getPanel() {
+		return panel;
+	}
+	
+	public void refreshFrame() {
+		frame.setVisible(false);
+		frame.setVisible(true);
+	}
 }
