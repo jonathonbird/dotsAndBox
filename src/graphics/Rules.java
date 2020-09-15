721542c99752b7c3299376ac8717c6b17fb66790
Lindalee-Conradie <lee.conradie101@gmail.com>
2020-09-15 14:37:34 +0200

    rule panel

--- src/graphics/Rules.java
+++ src/graphics/Rules.java
@@ -1,5 +1,12 @@
 package graphics;
+import javax.swing.*;
+import java.awt.*;
 
 public class Rules {
-
+    public void setRulePanel() {
+        JPanel rulePanel = new JPanel();
+        JLabel ruleLabel = new JLabel();
+        ruleLabel.setText("Starting with an empty grid of dots, players take turns, adding a single horizontal or vertical line between two unjoined adjacent dots. A player who completes the fourth side of a 1 × 1 box earns one point and takes another turn. (The points are typically recorded by placing in the box an identifying mark of the player, such as an initial). The game ends when no more lines can be placed. The winner of the game is the player with the most points.");
+        rulePanel.add(ruleLabel);
+    }
 }
