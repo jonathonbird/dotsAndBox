923df530019bedd37341911c17405d403e690489
HusamAQ <56165481+HusamAQ@users.noreply.github.com>
2020-09-15 15:07:27 +0200

    V1 Path added

--- src/graphics/Paths.java
+++ src/graphics/Paths.java
@@ -1,7 +1,10 @@
 package graphics;
 
+
+//MAKE SURE TO CHANGE THIS TO UR RESPECTIVE FILE PATH ON YOUR COMPUTER!!
+//AND WHEN YOU COMMIT DO NOT COMMIT THIS BECAUSE IT RUINS THE GRAPHICS FOR THE REST OF THE GROUP
 public interface Paths {
-	public static final String BACKGROUND_MAIN="";
+	public static final String BACKGROUND_MAIN="C:\\Users\\husam\\OneDrive\\Documents\\GitHub\\dotsAndBox\\resources\\basicBG.png";
 	public static final String BACKGROUND_PLAY="";
 	public static final String BACKGROUND_RULES="";
 	public static final String BACKGROUND_CREDITS="";
