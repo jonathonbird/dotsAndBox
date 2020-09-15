98130f7eee9f3eb8fb2f6ee6866b34695715884c
HusamAQ <56165481+HusamAQ@users.noreply.github.com>
2020-09-15 14:15:40 +0200

    Introduced the strings for the paths for the images to be used

--- src/graphics/Paths.java
+++ src/graphics/Paths.java
@@ -1,5 +1,23 @@
 package graphics;
 
 public interface Paths {
+	public static final String BACKGROUND_MAIN="";
+	public static final String BACKGROUND_PLAY="";
+	public static final String BACKGROUND_RULES="";
+	public static final String BACKGROUND_CREDITS="";
+	
+	public static final String BUTTON_PLAY="";
+	public static final String BUTTON_RULES="";
+	public static final String BUTTON_CREDITS="";
+	
+	public static final String BUTTON_BACK="";
+	public static final String BUTTON_START="";
+	
+	public static final String BUTTON_HUMAN="";
+	public static final String BUTTON_BOT="";
+	public static final String BUTTON_SIZE1="";
+	public static final String BUTTON_SIZE2="";
+	public static final String BUTTON_SIZE3="";
 
+	
 }
