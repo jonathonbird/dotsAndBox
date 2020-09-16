a14ff153f3f28474b254603a38d6c24a4e1891af
HusamAQ <56165481+HusamAQ@users.noreply.github.com>
2020-09-16 17:14:54 +0200

    Updated paths and introduced new paths and images

--- src/graphics/Paths.java
+++ src/graphics/Paths.java
@@ -4,14 +4,14 @@ package graphics;
 //MAKE SURE TO CHANGE THIS TO UR RESPECTIVE FILE PATH ON YOUR COMPUTER!!
 //AND WHEN YOU COMMIT DO NOT COMMIT THIS BECAUSE IT RUINS THE GRAPHICS FOR THE REST OF THE GROUP
 public interface Paths {
-	public static final String BACKGROUND_MAIN="C:\\Users\\husam\\OneDrive\\Documents\\GitHub\\dotsAndBox\\resources\\basicBG.png";
-	public static final String BACKGROUND_PLAY="";
-	public static final String BACKGROUND_RULES="";
-	public static final String BACKGROUND_CREDITS="";
+	public static final String BACKGROUND_MAIN="C:\\Users\\husam\\OneDrive\\Documents\\GitHub\\dotsAndBox\\resources\\MainBackground.png";
+	public static final String BACKGROUND_PLAY="C:\\Users\\husam\\OneDrive\\Documents\\GitHub\\dotsAndBox\\resources\\GeneralBackground.png";
+	public static final String BACKGROUND_RULES="C:\\Users\\husam\\OneDrive\\Documents\\GitHub\\dotsAndBox\\resources\\GeneralBackground.png";
+	public static final String BACKGROUND_CREDITS="C:\\Users\\husam\\OneDrive\\Documents\\GitHub\\dotsAndBox\\resources\\GeneralBackground.png";
 	
-	public static final String BUTTON_PLAY="";
-	public static final String BUTTON_RULES="";
-	public static final String BUTTON_CREDITS="";
+	public static final String BUTTON_PLAY="C:\\Users\\husam\\OneDrive\\Documents\\GitHub\\dotsAndBox\\resources\\Play.png";
+	public static final String BUTTON_RULES="C:\\Users\\husam\\OneDrive\\Documents\\GitHub\\dotsAndBox\\resources\\HowtoPlay.png";
+	public static final String BUTTON_CREDITS="C:\\Users\\husam\\OneDrive\\Documents\\GitHub\\dotsAndBox\\resources\\Credits.png";
 	
 	public static final String BUTTON_BACK="";
 	public static final String BUTTON_START="";
