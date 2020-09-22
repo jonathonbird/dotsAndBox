4baab159ce844388924b8f4d5686bfc832c4446b
HusamAQ <56165481+HusamAQ@users.noreply.github.com>
2020-09-22 20:31:48 +0200

    added selection possibility to buttons

--- src/graphics/PlayMenu.java
+++ src/graphics/PlayMenu.java
@@ -6,8 +6,7 @@ import java.awt.event.ActionListener;
 
 public class PlayMenu implements Menu{
 	private static Menu instance=null;
-
-	private JFrame frame;
+	
 	private JPanel playMenuPanel;
 	private JButton back;
 	private JButton play;
@@ -16,8 +15,9 @@ public class PlayMenu implements Menu{
 	private JButton size1;
 	private JButton size2;
 	private JButton size3;
-	private JLabel player;
-	private JLabel size;
+	
+	private int opt=0;
+	private int size=1;
 
 
 	private PlayMenu() {
@@ -53,17 +53,25 @@ public class PlayMenu implements Menu{
 	}
 
 	private void setUpPlayer(){
-		human=Button(Paths.BUTTON_HUMAN);
+		human=Button(Paths.BUTTON_HUMAN_SELECTED);
 		bot=Button(Paths.BUTTON_BOT);
 		
 		bot.setLocation(336,128);
 		human.setLocation(164,128);
-		bot.addActionListener(new ActionListener(){ public void actionPerformed(ActionEvent e){}});
-		human.addActionListener(new ActionListener(){ public void actionPerformed(ActionEvent e){}});
+		bot.addActionListener(new ActionListener(){ public void actionPerformed(ActionEvent e){
+			opt=1;
+			setIcon(bot, Paths.BUTTON_BOT_SELECTED);
+			setIcon(human, Paths.BUTTON_HUMAN);
+		}});
+		human.addActionListener(new ActionListener(){ public void actionPerformed(ActionEvent e){
+			opt=0;
+			setIcon(bot, Paths.BUTTON_BOT);
+			setIcon(human, Paths.BUTTON_HUMAN_SELECTED);
+		}});
 	}
 
 	private void setUpBoard(){
-		size1=Button(Paths.BUTTON_SIZE1);
+		size1=Button(Paths.BUTTON_SIZE1_SELECTED);
 		size2=Button(Paths.BUTTON_SIZE2);
 		size3=Button(Paths.BUTTON_SIZE3);
 		
@@ -71,6 +79,9 @@ public class PlayMenu implements Menu{
 		size2.setLocation(323,329);
 		size3.setLocation(475,329);
 		
+		size1.addActionListener(new ActionListener(){ public void actionPerformed(ActionEvent e){setActiveSize(1);}});
+		size2.addActionListener(new ActionListener(){ public void actionPerformed(ActionEvent e){setActiveSize(2);}});
+		size3.addActionListener(new ActionListener(){ public void actionPerformed(ActionEvent e){setActiveSize(3);}});
 	}
 	
 	private JButton Button(String path) {
@@ -83,12 +94,45 @@ public class PlayMenu implements Menu{
 		return button;
 	}
 	
+	private void setActiveSize(int s) {
+		if(s==size) return;
+		switch (size){
+		case 1:
+			setIcon(size1, Paths.BUTTON_SIZE1);
+			break;
+		case 2:
+			setIcon(size2, Paths.BUTTON_SIZE2);
+			break;
+		case 3:
+			setIcon(size3, Paths.BUTTON_SIZE3);
+			break;
+		}
+		
+		switch (s){
+		case 1:
+			setIcon(size1, Paths.BUTTON_SIZE1_SELECTED);
+			break;
+		case 2:
+			setIcon(size2, Paths.BUTTON_SIZE2_SELECTED);
+			break;
+		case 3:
+			setIcon(size3, Paths.BUTTON_SIZE3_SELECTED);
+			break;
+		}
+		size=s;
+	}
+	
+	private void setIcon(JButton button, String path) {
+		ImageIcon icon = new ImageIcon(path);
+		button.setIcon(icon);
+	}
+	
 	public static Menu getInstance() {
 		if(instance==null) instance=new PlayMenu();
 		return instance;
 	}
 
 	public void setUpActionListeners(MenuBasic base,Menu Main) {
-	    	Menu.setNavigationTo(base, this.back, Main);
-	    }
+	    Menu.setNavigationTo(base, this.back, Main);
+	}
 }
